f4e414d79f4b182ea9c4c30b26fd2b6d48b583b7
Your Name <you@example.com>
2018-02-03 06:25:01 -0400

    created project with dependecies

--- README.md
+++
@@ -1,2 +0,0 @@
-# panelControl
-This app will able to administrate users and generate codes as licences of some programs.
---
+++ src/main/java/com/key/licence/KeyLicenceApplication.java
@@ -0,0 +1,12 @@
+package com.key.licence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyLicenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyLicenceApplication.class, args);
+	}
+}
