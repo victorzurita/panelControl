a3a17d809db6f9754c710236ce34cca971bd3b12
Your Name <you@example.com>
2018-03-25 15:20:21 -0400

    Removed files from root

--- src/main/java/com/key/licence/Core/parsers/IParser.java
+++
@@ -1,9 +0,0 @@
-package com.key.licence.Core.parsers;
-
-/**
- * Created by jrafa on 2/4/2018.
- */
-public interface IParser<E, D> {
-    D parserEntityToDto(E entity);
-    E parserDtoToEntity(D dto);
-}
--- src/main/java/com/key/licence/Dal/repositories/interfaces/IRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.key.licence.Dal.repositories.interfaces;
-
-import com.key.licence.Dal.models.Role;
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by jrafa on 2/4/2018.
- */
-public interface IRoleRepository extends CrudRepository<Role, Integer>{}
